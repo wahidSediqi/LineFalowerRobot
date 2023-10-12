8511f338924d93d2e92f9b005b40470addcd5826
Wahid Sediqi <54534266+Azad888@users.noreply.github.com>
2023-10-12 10:59:24 +0200

    implemetert enkelt PID og prøvd å utvikle den.

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,27 @@ int enA = 3;
 int enB = 5;
 int Emitter_pin = 8;
 
-QTRSensors qtr;
+// Define PID parameters
+double Kp = 2.0;  // Proportional gain
+double Ki = 0.0;  // Integral gain
+double Kd = 1.0;  // Derivative gain
+double dt = 0.01;  // Time step (adjust as needed)
 
-const uint8_t SensorCount = 5;
+
+// Advanced PID parameters
+double maxMotorSpeed = 255;  // Maximum motor speed
+double maxIntegral = maxMotorSpeed / Ki;  // Maximum integral term
+double integral = 0;
+double lastError = 0;
+QTRSensors qtr;
+//
+const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount];
 void setup() {
 // write your initialization code here
 //sensor Array initialization
     qtr.setTypeRC();
-    qtr.setSensorPins((const uint8_t[]) {9, 10, 11, 12, 13}, SensorCount);
+    qtr.setSensorPins((const uint8_t[]) {6, 7, 8, 9, 10, 11, 12, 13}, SensorCount);
     qtr.setEmitterPin(Emitter_pin);
 //serial cammunication
     Serial.begin(9600);
@@ -58,8 +70,25 @@ void setup() {
 void loop() {
 // write your code here
     uint16_t position = qtr.readLineBlack(sensorValues);
+    // Calculate error (adjust as needed based on sensor arrangement)
+    double error = (sensorValues[0] - sensorValues[7]) + (sensorValues[1] - sensorValues[6]) +
+                   (sensorValues[2] - sensorValues[5]) + (sensorValues[3] - sensorValues[4]);
+    integral += Ki * error * dt;
+    integral = constrain(integral, -maxIntegral, maxIntegral);
+    // Calculate derivative
+    double derivative = (error - lastError) / dt;
 
-    int gain = map(sensorValues,0,2500,0,255);
+    // Calculate motor speeds
+    double motorSpeed = Kp * error + integral + Kd * derivative;
+    double leftMotorSpeed = maxMotorSpeed + motorSpeed;
+    double rightMotorSpeed = maxMotorSpeed - motorSpeed;
 
+    analogWrite(enA, leftMotorSpeed);
 
+    analogWrite(enB, rightMotorSpeed);
+
+    lastError = error;
+
+    // Delay for the time step
+    delay(dt * 1000);
 }
